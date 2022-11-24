87f16a0fd608ab3217b31a98f44c38f6ea9c116d
lilansim <115713010+lilansim@users.noreply.github.com>
2022-11-24 19:07:46 +0300

    Database Modified

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import pymysql
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'fraud',
         'USER': 'root',
         'HOST': 'localhost',
